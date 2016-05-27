09d9b36a68f552fb163ffa509706e9a449f592f2
Blaine Nelson <blaine.m.nelson@gmail.com>
2016-05-27 08:49:48 -0400

    Changes to make web-mode work with React JSX jazz

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -10,7 +10,10 @@
  '(custom-safe-themes
    (quote
     ("20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "acb039d6f2c41b3bd852b448351b2979f44ef488026c95dd5228d2f6da57f574" default)))
- '(js2-basic-offset 4))
+ '(js2-basic-offset 2)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -21,5 +24,3 @@
  '(magit-item-highlight ((t (:background "grey44"))))
  '(minibuffer-prompt ((t (:foreground "brown")))))
 ;;; custom.el ends here
-
-
